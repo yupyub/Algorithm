6f321b86df5e50ccdf5f4023bdc5ce435106ce17
yupyub <sungyup5003@gmail.com>
2019-12-03 09:10:28 +0000

    Algorithm Training : Competitive Programming

---
+++ Training/convention.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_DEPRECATE // Hide some compile warning message
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+#define INF 1000000000 // 10^9
+
+// Usage of memset
+memset(memo,-1,sizeof memo); // initialize memo table to -1
+memset(arr,0,sizeof arr); // initialize arr to 0
+
+// Shorthand syntax
+ans = a?b:c; // if(a) ans = b; else ans = c;
+index = (index+1)%n; // index++; if(index == n) n = 0;
+index = (index+n-1)%n; // index--; if(index < 0) n = n-1;
+int ans = (int)((double)d+0.5); // Rounds
+
+
+
