0e7a33a35e6cc8c79e6e1a2d13b8296c81549939
yupyub <sungyup5003@gmail.com>
2020-02-17 07:30:25 +0000

    .

--- boj/etc/5639.cpp
+++ boj/etc/5639.cpp
@@ -1,21 +1,18 @@
 #include <cstdio>
-int tree[10000];
-int n;
-int f(int i){
-	if(i == n-1) return i;
-	if(tree[i]>tree[i+1]){
-		int nx = f(i+1);
-		printf("%d ",tree[i]);
-		f(nx+1);
-	}
-	else if(tree[i-1]<tree[i+1]){
-		printf("%d ",tree[i]);
-		return i;
-	}
+int arr[10000];
+void f(int l,int r){
+	if(l>r) return;
+	int nw = l;
+	int s = l,e = r;
+	while(arr[s]>=arr[nw]) s++;
+	while(arr[e]>arr[nw]) e--;
+	f(s,e);
+	f(e+1,r);
+	printf("%d\n",arr[nw]);
 }
 int main(){
-	while(scanf("%d",&tree[n++]) != EOF){
-		if(tree[n-1] == -1) break;
-	}
-	f(0);
+	int n = 0;
+	while(scanf("%d",&arr[n++]) != EOF)
+		if(arr[n-1] == -1) break;
+	f(0,n-2);
 }
