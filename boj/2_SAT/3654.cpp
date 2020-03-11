aabf173fb07f9d4066b621207644bdaffae50577
yupyub <sungyup5003@gmail.com>
2020-03-11 11:31:35 +0000

    SOLVE 3654git add *git add *git add *git add *git add *!

--- boj/2_SAT/3654.cpp
+++ boj/2_SAT/3654.cpp
@@ -5,9 +5,7 @@
 #include <algorithm>
 #include <vector>
 #include <stack>
-#define V_MAX 500002 // True False 2경우의 Vertex 필요 (0~V*2+1 index 사용)
-#define NUMBERING(x) ((x)<0?-(x)*2:(x)*2+1) // 1->3, -1->2 // 2->5, -2->4 // *** 0->0,1 ***
-#define OPPOSITE(x) ((x)%2?(x)-1:(x)+1)     // 3->2, 2->3  // 5->4, 4->5  // *** 1->0, 0->1 ***
+#define V_MAX 1000002 
 using namespace std;
 vector<vector<int> >graph;
 stack<int> dfs_stack;
@@ -18,7 +16,15 @@ int N,M;
 int order = 1,ans,numbering = 0;
 int dx[] = {-1,0,1,0};
 int dy[] = {0,1,0,-1};
+int ddx[] = {2,0,1,1};
+int ddy[] = {0,2,1,-1};
 char map[500][500];
+int NUMBERING(int k,int p){
+	return k*4+p;
+}
+int OPPOSITE(int k){
+	return 4*(k/4)+(k+2)%4;
+}
 int Tarjan(int node){
 	dfs_order[node] = order++;
 	dfs_stack.push(node);
@@ -43,48 +49,62 @@ int Tarjan(int node){
 	return min_order;
 }
 void add_graph_B(int x,int y){
-	int nxt,k;
-	int ops = 0,odd = -1,even = -1; // map 범위를 벗어나는 명제인지 확인
+	int k = M*x+y;
 	for(int i = 0;i<4;i++){
 		int nx = x+dx[i];
 		int ny = y+dy[i];
 		if(nx<0||ny<0||nx>=N||ny>=M || map[nx][ny] !='W'){
-			ops = (i+2)%4;
-			if(ops%2)
-				odd = ops;
-			else
-				even = ops;
+			int ops = (i+2)%4;
 			nx = x+dx[ops];
 			ny = y+dy[ops];
 			if(map[nx][ny] != 'W'){
 				ans = 1;
 				return;
 			}
-			k = NUMBERING(M*x+y);
-			if(ops == 0||ops == 3)
-				graph[OPPOSITE(k)].push_back(k);
-			else
-				graph[k].push_back(OPPOSITE(k));
+			switch (ops){
+				case 0: 
+					graph[NUMBERING(k,2)].push_back(NUMBERING(k,0));
+					break;
+				case 1:
+					graph[NUMBERING(k,1)].push_back(NUMBERING(k,3));
+					break;
+				case 2:
+					graph[NUMBERING(k,0)].push_back(NUMBERING(k,2));
+					break;
+				case 3:
+					graph[NUMBERING(k,3)].push_back(NUMBERING(k,1));
+					break;
+			}
 		}
 	}
-	for(int i = 0;i<2;i++){
-		if(odd!=-1&&i%2)
+	for(int i = 0;i<4;i++){
+		int nx = x+ddx[i];
+		int ny = y+ddy[i];
+		int a = k;
+		int b = M*nx+ny;
+		if(nx<0||ny<0||nx>=N||ny>=M || map[nx][ny] !='B')
 			continue;
-		if(even!=-1&&(!(i%2)))
-			continue;
-		int nx = x+dx[i];
-		int ny = y+dy[i];
-		int a = NUMBERING(M*nx+ny);
-		int nnx = x+dx[i+2];
-		int nny = y+dy[i+2];
-		int b = NUMBERING(M*nnx+nny);
-		if(i%2){
-			graph[OPPOSITE(b)].push_back(OPPOSITE(a));
-			graph[a].push_back(b);
-		}
-		else{
-			graph[OPPOSITE(a)].push_back(OPPOSITE(b));
-			graph[b].push_back(a);
+		switch(i){
+			case 0:
+				graph[NUMBERING(b,0)].push_back(NUMBERING(a,0));
+				graph[NUMBERING(a,2)].push_back(NUMBERING(b,2));
+				break;
+			case 1:
+				graph[NUMBERING(b,1)].push_back(NUMBERING(a,1));
+				graph[NUMBERING(a,3)].push_back(NUMBERING(b,3));
+				break;
+			case 2:
+				graph[NUMBERING(b,0)].push_back(NUMBERING(a,1));
+				graph[NUMBERING(a,3)].push_back(NUMBERING(b,2));
+				graph[NUMBERING(b,1)].push_back(NUMBERING(a,0));
+				graph[NUMBERING(a,2)].push_back(NUMBERING(b,3));
+				break;
+			case 3:
+				graph[NUMBERING(b,0)].push_back(NUMBERING(a,3));
+				graph[NUMBERING(a,1)].push_back(NUMBERING(b,2));
+				graph[NUMBERING(b,3)].push_back(NUMBERING(a,0));
+				graph[NUMBERING(a,2)].push_back(NUMBERING(b,1));
+				break;
 		}
 	}
 }
@@ -95,7 +115,7 @@ int main(){
 		scanf("%d %d",&N,&M);
 		int V = N*M-1;
 		graph.clear();
-		graph.resize(V*2+2);
+		graph.resize(V*4+4);
 		int W_num = 0,B_num = 0;
 		for(int i = 0;i<N;i++)
 			for(int j = 0;j<M;j++)
@@ -117,24 +137,21 @@ int main(){
 		}
 		order = 1;
 		numbering = 0;
-		for(int i = 0;i<=V*2+1;i++)
+		for(int i = 0;i<V*4+4;i++)
 			finish[i] = dfs_order[i] = 0;
-		for(int i = 0;i<=V*2+1;i++){
+		for(int i = 0;i<V*4+4;i++){
 			if(!finish[i])
 				Tarjan(i);
 		}
 		for(int i = 0;i<=V;i++){
-			if(SCC_num[NUMBERING(i)] == SCC_num[OPPOSITE(NUMBERING(i))]){
+			if(SCC_num[NUMBERING(i,0)] == SCC_num[NUMBERING(i,2)]){
 				ans = 1;
 				break;
 			}
-		}
-		for(int i = 0;i<=V*2+1;i++){
-			printf("%d) ",i);
-			for(int j : graph[i]){
-				printf("%d, ",j);
+			if(SCC_num[NUMBERING(i,1)] == SCC_num[NUMBERING(i,3)]){
+				ans = 1;
+				break;
 			}
-			printf("\n");
 		}
 		if(ans)
 			printf("NO\n");
