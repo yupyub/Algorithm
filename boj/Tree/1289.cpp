d5a0e14eff2f798c36d93ef6f7f975ab26405087
yupyub <sungyup5003@gmail.com>
2020-03-16 07:42:34 +0000

    .

--- boj/Tree/1289.cpp
+++ boj/Tree/1289.cpp
@@ -8,22 +8,29 @@ int N;
 ll cost[100001];
 int visit[100001];
 vector<pair<int,ll> > tree[100001];
-ll f(int n){
+ll ans = 0;
+void f(int n){
+	ll tot = 0,ptot = 0;
+	ll tmp = 0;
 	for(auto nx : tree[n]){
 		int nxt = nx.first;
+		ll nxc = nx.second;
 		if(!visit[nxt]){
-			visit[nxt] = n;
+			visit[nxt] = 1;
 			f(nxt);
+			tmp = ((cost[nxt]+1)*nxc)%MOD;
+			tot = (tot + tmp)%MOD;
+			ptot = (ptot + tmp*tmp)%MOD;
 		}
 	}
-	ll tot = 0,ptot = 0;
-	for(auto nx : tree[n]){
-		int nxt = nx.first;
-		int nxc = nx.second;
-		if(visit[nxt] != n)
-			continue;
-		
-	}
+	cost[n] = tot;
+	tmp = tot*tot-ptot;
+	//(ab+ac+ad+bc+bd+cd) = ((a+b+c+d)^2-(a^2+b^2+c^2+d^2))/2 이용 O(k^2) 을 O(k)로 단축
+	if(tmp%2)
+		tmp = ((tmp+MOD)/2)%MOD;
+	else
+		tmp = tmp/2;
+	ans =(((tmp+cost[n])%MOD)+ans)%MOD;
 }
 int main(){
 	int a,b;
@@ -35,5 +42,6 @@ int main(){
 		tree[b].push_back(make_pair(a,c));
 	}
 	visit[1] = 1;
-	printf("%lld",f(1));
+	f(1);
+	printf("%lld",ans);
 }
