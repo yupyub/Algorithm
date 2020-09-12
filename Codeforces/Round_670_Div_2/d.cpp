b2dae3ade9263e66937e92f4dde5e1cafebdf38e
yupyub <steve5003@naver.com>
2020-09-12 22:23:39 +0900

    go

---
+++ Codeforces/Round_670_Div_2/d.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <tuple>
+#include <functional>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		
+	}
+}
