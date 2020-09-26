287358361676fd0775be71374f5fa307507ba2d3
yupyub <steve5003@naver.com>
2020-09-26 15:21:29 +0900

    go

--- boj/temp/temp.cpp
+++ boj/temp/temp.cpp
@@ -16,6 +16,16 @@ using namespace std;
 typedef long long ll;
 const int INF = 987654321;
 
+void H(int n,char from,char to,char aux){
+    if(n == 0)
+        return;
+    H(n-1,from,aux,to);
+    printf("%c -> %c\n",from,to);
+    H(n-1,aux,to,from);
+}
+
 int main(){
-    
+    int n;
+    scanf("%d",&n);
+    H(n,'A','C','B');
 }
