d73e136a02a51f9c71716917fa27507ed71f4953
yupyub <steve5003@naver.com>
2020-09-28 17:51:17 +0900

    go

--- Codeforces/Round_673_Div_2/a.cpp
+++ Codeforces/Round_673_Div_2/a.cpp
@@ -14,11 +14,27 @@ typedef pair<int,int> ii;
 const int INF = 987654321;
 int dx[] = {-1,0,1,0};
 int dy[] = {0,1,0,-1};
-
+int arr[1000];
 int main(){
 	int t;
 	scanf("%d",&t);
 	while(t--){
-		
+		int n,k;
+		scanf("%d %d",&n,&k);
+		int mini = INF;
+		int idx = 0;
+		for(int i = 0;i<n;i++){
+			scanf("%d",&arr[i]);
+			if(mini>arr[i]){
+				mini = arr[i];
+				idx = i;
+			}
+		}
+		int ans = 0;
+		for(int i = 0;i<n;i++){
+			if(i == idx) continue;
+			ans += (k-arr[i])/mini;
+		}
+		printf("%d\n",ans);
 	}
 }
--- Codeforces/Round_673_Div_2/b.cpp
+++ Codeforces/Round_673_Div_2/b.cpp
@@ -14,11 +14,22 @@ typedef pair<int,int> ii;
 const int INF = 987654321;
 int dx[] = {-1,0,1,0};
 int dy[] = {0,1,0,-1};
-
+pair<int,int> arr[100000];
+int ans[100000];
 int main(){
 	int t;
 	scanf("%d",&t);
 	while(t--){
-		
+		int n,m;
+		scanf("%d %d",&n,&m);
+		for(int i = 0;i<n;i++){
+			scanf("%d",&arr[i].first);
+			arr[i].second = i;
+			ans[i] = 0;
+		}
+		sort(arr,arr+n);
+		for(int i = 0;i<n;i++){
+			ans[i] = 
+		}
 	}
 }
